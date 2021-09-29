09ae0a2f3e6e778ffd7dd2e83b41b8762cb372d3
Stephen Louis <stephenlouis34e@gmail.com>
2021-09-29 09:56:41 +0100

    adding a new project

---
+++ app/js/script.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('Open Hamburger');
+
+
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        body.classList.remove('nonscroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+
+    });
+    }
+       
+    
+        
+else{ // Open Hamburger Menu 
+    body.classList.add('nonscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+
+    });
+}
+});
+    
+
